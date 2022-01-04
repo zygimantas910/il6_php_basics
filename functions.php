421681bbccf766a80586fbc128db18b3ca50c9f7
Mac <mac@Macs-MacBook-Pro.local>
2022-01-04 20:32:45 +0200

    Second commit 2

--- functions.php
+++ functions.php
@@ -1,3 +1,17 @@
 <?php
-$productprice = 12;
-$discount = 21;
+$userEmail = 'betkoksemail.lt';
+if(isEmailValid($userEmail)){
+    echo clearEmail($userEmail);
+}else{
+    echo 'Emailas nevalidus';
+}
+function isEmailValid($email){
+    if (strpos($email, '@')) {
+        return true;
+    }
+    return false;
+}
+function clearEmail($email){
+    $emailLowercases = strtolower($email);
+    return trim($emailLowercases);
+}
