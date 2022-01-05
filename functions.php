cadd1b6bc07a0d3c383dffc3874062a7ec951679
Mac <mac@Macs-MacBook-Pro.local>
2022-01-05 18:34:07 +0200

    Forms

--- functions.php
+++ functions.php
@@ -1,17 +1,36 @@
 <?php
-$userEmail = 'betkoksemail.lt';
-if(isEmailValid($userEmail)){
-    echo clearEmail($userEmail);
-}else{
-    echo 'Emailas nevalidus';
+// $userEmail = 'betkoksemail.lt';
+// if(isEmailValid($userEmail)){
+//     echo clearEmail($userEmail);
+// }else{
+//     echo 'Emailas nevalidus';
+// }
+// function isEmailValid($email){
+//     if (strpos($email, '@')) {
+//         return true;
+//     }
+//     return false;
+// }
+// function clearEmail($email){
+//     $emailLowercases = strtolower($email);
+//     return trim($emailLowercases);
+// }
+
+$name = 'Zygimantas';
+$surname = 'Kalniunas';
+$number = rand(0 , 100);
+
+echo getNickName($name, $surname, $number);
+
+function getNickName($name, $surname, $number){
+    return strtolower(substr($name, 0, 3) . substr($surname, 0,3) . substr($number, 0,3));
 }
-function isEmailValid($email){
-    if (strpos($email, '@')) {
-        return true;
-    }
-    return false;
+
+$title = 'Jusu kazkokia tai antraste';
+$slug = getSlug($title);
+
+function getSlug($title) {
+    return strtolower(str_replace(" ", "-", $title));
 }
-function clearEmail($email){
-    $emailLowercases = strtolower($email);
-    return trim($emailLowercases);
-}
+
+echo $slug;
